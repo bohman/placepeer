ed895df72962c17eb4cbf157eb25969b2defc25c
Linus Bohman <linus@linusbohman.se>
2012-03-04 13:50:37 +0100

    Initial commit. Basic request is sent to Twitter, and the data is placed on a map.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Placepeer</title>
+  <link rel='stylesheet' id='placepeer-css'  href='style.css' type='text/css' media='all' />
+  <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
+  <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCBZ5stPtmfRqLgbtjwd-SCGApkG_OlnDU&sensor=false"></script>
+  <script type='text/javascript' src='ui.js'></script>
+</head>
+<body>
+
+  <div id="map">
+    <div id="map-canvas"></div>
+  </div>
+
+  <div id="timeline">
+    timeline
+  </div>
+
+</body>
+</html>
