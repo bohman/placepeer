4753316453f43f4bbf39657301a43bccbc930465
Olof Johansson <olof@oddhill.se>
2012-03-18 11:59:46 +0100

    Daniel's changes that has been downloaded from the server.

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>Placepeer</title>
-  <link rel='stylesheet' id='placepeer-css'  href='style.css' type='text/css' media='all' />
+  <link rel='stylesheet' id='placepeer-css'  href='css/style.css' type='text/css' media='all' />
   <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
   <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCBZ5stPtmfRqLgbtjwd-SCGApkG_OlnDU&sensor=false"></script>
   <script type='text/javascript' src='ui.js'></script>
+  <script type='text/javascript' src='script.js'></script>
 </head>
 <body>
-
+  
+  <div id="header">
+    <a id="logo" href=""><img src="../graphics/logo.png"></a>
+    <div class="dropdown">
+      List
+    </div>
+  </div>
+  
+  <div id="list-wrapper">
+    en list-wrapper!
+  </div>
+  
   <div id="map">
     <div id="map-canvas"></div>
   </div>
@@ -18,5 +30,8 @@
     timeline
   </div>
 
+  <div id="footer">
+    footer
+  </div>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function() {
+  $('div.dropdown').click(function() {
+	  $('div#list-wrapper').toggle();
+  });
+});
