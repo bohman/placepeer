fe58c88e643f5ca03737a9cdcc8dfc98a46aca79
Linus Bohman <linus@linusbohman.se>
2012-03-18 16:52:29 +0100

    Commented out temporary areas

--- index.php
+++ index.php
@@ -25,30 +25,31 @@
     <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f53724c3a21b3bc"></script>
   </div>
   
-  <div id="list-wrapper">
+  <!--<div id="list-wrapper">
     en list-wrapper!
-  </div>
+  </div>-->
   
   <div id="map">
     <div id="map-canvas"></div>
   </div>
 
-  <div id="timeline">
+  <!--<div id="timeline">
     timeline
-  </div>
+  </div>-->
 
-  <div id="footer">
+  <!--<div id="footer">
     footer
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-29696179-1']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
-  </div>
+  </div>-->
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29696179-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </body>
 </html>
