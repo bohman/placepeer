6627bdb3d39446f3e92b1ba9d2b4d58406c9c0b1
Olof Johansson <olof@oddhill.se>
2012-05-13 15:08:28 +0200

    Magically fetch images from external links.
    This needs a lot more comments, code improvements, security fixes etc and so on.
    
    But I'm not allowed to play anymore today, so I'll probably do this at a later stage.

---
+++ get.php
@@ -0,0 +1,25 @@
+<?php
+
+  if ($_GET['url']) {
+    $curl = curl_init($_GET['url']);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    //parsing begins here:
+    $doc = new DOMDocument();
+    @$doc->loadHTML($response);
+    $metas = $doc->getElementsByTagName('meta');
+
+    $return = array();
+    foreach ($metas as $meta) {
+      if ($meta->getAttribute('property') == 'og:image') {
+        $return['image'] = $meta->getAttribute('content');
+      }
+    }
+    
+    print json_encode($return);
+  }
+
+?>
