d4b4dde412006bb3adc42c30b67d315968cfe579
Björn Albertsson <bjorn.albertsson@gmail.com>
2012-08-12 11:43:03 +0200

    Removed AddThis mail and +

--- index.php
+++ index.php
@@ -24,8 +24,6 @@
     <div class="addthis_toolbox addthis_32x32_style addthis_default_style">
         <a class="addthis_button_facebook"></a>
         <a class="addthis_button_twitter"></a>
-        <a class="addthis_button_email"></a>
-        <a class="addthis_button_compact"></a>
     </div>
     <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f53724c3a21b3bc"></script>
   </div>
